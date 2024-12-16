375b1580e8cae8647716012b8dfb8a38bb561e2a
GOLF <pakin.n@muze.co.th>
2024-12-16 23:52:29 +0700

    add: comment service

---
+++ src/comment/comment.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository, In } from "typeorm";
+import { CommentEntity } from "../entities/comment.entity";
+import { PostEntity } from "../entities/post.entity";
+
+@Injectable()
+export class CommentService {
+  constructor(
+    @InjectRepository(CommentEntity)
+    private commentRepo: Repository<CommentEntity>
+  ) {}
+
+  async findAll(postIds: string[]): Promise<CommentEntity[]> {
+    const queryOptions: any = {
+      order: {
+        createdAt: "DESC",
+      },
+    };
+
+    // Add where condition only if postIds has a length
+    if (postIds.length > 0) {
+      queryOptions.where = {
+        post: { id: In(postIds) },
+      };
+    }
+
+    const comments = await this.commentRepo.find(queryOptions);
+
+    return comments;
+  }
+}
