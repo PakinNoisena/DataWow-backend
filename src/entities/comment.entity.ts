5131d6fc3450041a14e2c97b66a874cd3acdcd17
GOLF <pakin.n@muze.co.th>
2024-12-16 23:51:56 +0700

    add: comment entities

---
+++ src/entities/comment.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { UserEntity } from "./user.entity";
+import { PostEntity } from "./post.entity";
+
+@Entity("comments")
+export class CommentEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  message!: string;
+
+  @ManyToOne(() => UserEntity, { eager: true })
+  @JoinColumn({ name: "commented_by" })
+  commentedBy!: UserEntity;
+
+  @ManyToOne(() => PostEntity, { eager: true })
+  @JoinColumn({ name: "post" })
+  post!: PostEntity;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt!: Date;
+}
