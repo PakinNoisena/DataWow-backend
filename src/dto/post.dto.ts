6feaf6800eb8a591344462eb9741c52026725a03
GOLF <pakin.n@muze.co.th>
2024-12-17 02:04:49 +0700

    add: dto for update post

--- src/dto/post.dto.ts
+++ src/dto/post.dto.ts
@@ -1,4 +1,32 @@
 import { Expose } from "class-transformer";
+import { createZodDto } from "nestjs-zod";
+import { z } from "zod";
+
+const PostBodySchema = z.object({
+  title: z
+    .string()
+    .min(1, "Username cannot be empty")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Username cannot be null or undefined",
+    })
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description cannot be empty")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Description cannot be null or undefined",
+    })
+    .optional(),
+  communityId: z
+    .number()
+    .positive("Community ID must be greater than 0")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Community ID cannot be null or undefined",
+    })
+    .optional(),
+});
+
+export class PostBodyDto extends createZodDto(PostBodySchema) {}
 
 // Update this to reflect the type of comments
 export class PostRespDto {
