d793e9f5debf6edf3729175a4794d787b50908a1
GOLF <pakin.n@muze.co.th>
2024-12-16 23:53:32 +0700

    add: CommentService to PostModule

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostEntity } from "../entities/post.entity";
 import { PostController } from "./post.controller";
 import { PostService } from "./post.service";
+import { CommentEntity } from "../entities/comment.entity";
+import { CommunityEntity } from "../entities/community.entity";
+import { CommentService } from "../comment/comment.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PostEntity, CommentEntity, CommunityEntity]),
+  ],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, CommentService],
 })
 export class PostModule {}
