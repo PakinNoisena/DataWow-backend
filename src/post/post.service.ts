307345106d31d1adc77be3e635aa47b9440ecc7a
GOLF <pakin.n@muze.co.th>
2024-12-16 23:54:00 +0700

    fix: FindAll post service to return comments

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,12 +3,15 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { PostEntity } from "../entities/post.entity";
 import { PostRespDto } from "../dto/post.dto";
+import { CommentEntity } from "../entities/comment.entity";
+import { CommentService } from "../comment/comment.service";
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(PostEntity)
-    private postRepo: Repository<PostEntity>
+    private postRepo: Repository<PostEntity>,
+    private commentService: CommentService
   ) {}
 
   async findAll(search?: string): Promise<PostRespDto[]> {
@@ -27,20 +30,46 @@ export class PostService {
         "community.name",
       ])
       .orderBy("post.createdAt", "DESC");
-    // If a search term is provided, apply the LIKE query for the title
+
     if (search) {
       queryBuilder.where("post.title LIKE :title", { title: `%${search}%` });
     }
 
     const posts = await queryBuilder.getMany();
 
-    return posts.map((post) => {
+    const postResults = posts.map((post) => {
       const { owner, community, ...postData } = post;
       return {
         ...postData,
         owner: { username: owner.username },
         community: { name: community.name },
+        comments: [], // Initialize comments as an empty array
       };
     });
+
+    const postIds = postResults.map((post) => {
+      return post.id;
+    });
+
+    // Get all comments
+    const commentResults = await this.commentService.findAll(postIds);
+
+    // Add comments to each post in postResults
+    postResults.forEach((post) => {
+      // Filter comments for the current post
+      const postComments = commentResults.filter((com) => {
+        return com.post.id === post.id;
+      });
+
+      // Add the comments to the post if any
+      (post as any).comments = postComments.map((comment) => ({
+        id: comment.id,
+        message: comment.message,
+        createdAt: comment.createdAt,
+        commentedBy: comment.commentedBy.username,
+      }));
+    });
+
+    return postResults;
   }
 }
