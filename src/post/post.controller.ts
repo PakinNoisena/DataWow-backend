3dba0a1b7af02dcdc93036ca2f2a01531f5e7fbb
GOLF <pakin.n@muze.co.th>
2024-12-17 02:05:17 +0700

    add: controller for update and delete post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Query,
+  Put,
+  Param,
+  Body,
+  Headers,
+  Delete,
+} from "@nestjs/common";
 import { PostService } from "./post.service";
+import { PostBodyDto, PostRespDto } from "../dto/post.dto";
 import { PostEntity } from "../entities/post.entity";
-import { PostRespDto } from "../dto/post.dto";
 
 @Controller("/post")
 export class PostController {
@@ -11,5 +20,23 @@ export class PostController {
   async findAllPost(@Query("search") search?: string): Promise<PostRespDto[]> {
     const posts = await this.postService.findAll(search);
     return posts;
+  }
+
+  @Put("/:id")
+  async editPost(
+    @Param("id") postId: string,
+    @Headers("user-id") userId: string,
+    @Body() updateData: Partial<PostBodyDto>
+  ): Promise<PostRespDto> {
+    const updatedPost = await this.postService.edit(postId, userId, updateData);
+    return updatedPost;
+  }
+
+  @Delete("/:id")
+  async deletePost(
+    @Param("id") postId: string,
+    @Headers("user-id") userId: string
+  ): Promise<void> {
+    await this.postService.delete(postId, userId);
   }
 }
