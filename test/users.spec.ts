8a703ca0f452c464d5c3738ca041707986e83254
GOLF <pakin.n@muze.co.th>
2024-12-16 20:34:20 +0700

    add: unit test for signIn

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -2,6 +2,7 @@ import { NotFoundException } from "@nestjs/common";
 import { UsersController } from "../src/users/users.controller";
 import { UsersService } from "../src/users/users.service";
 import { Test, TestingModule } from "@nestjs/testing";
+import { UserBodyDto } from "../src/dto/users.dto";
 
 describe("UsersController", () => {
   let controller: UsersController;
@@ -10,6 +11,7 @@ describe("UsersController", () => {
   beforeEach(async () => {
     const serviceMock = {
       findUserByUsername: jest.fn(),
+      create: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -26,12 +28,7 @@ describe("UsersController", () => {
     service = module.get<UsersService>(UsersService);
   });
 
-  describe("db generalizer", () => {
-    const currentServiceDB = process.env.SERVICE_DB;
-    afterAll(() => {
-      process.env.SERVICE_DB = currentServiceDB;
-    });
-
+  describe("findUserByUsername", () => {
     it.each`
       caseName                                   | username          | shouldExist | expected
       ${"should return data that already exist"} | ${"Pakin1"}       | ${true}     | ${{ id: 1, username: "Pakin1", createdAt: new Date() }}
@@ -63,6 +60,47 @@ describe("UsersController", () => {
               message: `User with username '${username}' not found`,
               messageCode: 1001,
             })
+          );
+        }
+      }
+    );
+  });
+
+  describe("signIn", () => {
+    const currentServiceDB = process.env.SERVICE_DB;
+    afterAll(() => {
+      process.env.SERVICE_DB = currentServiceDB;
+    });
+
+    it.each`
+      caseName                               | username     | shouldExist | expectedUserReturn
+      ${"should return the existing user"}   | ${"Pakin1"}  | ${true}     | ${{ id: 1, username: "Pakin1", createdAt: new Date() }}
+      ${"should create and return new user"} | ${"NewUser"} | ${false}    | ${{ id: 2, username: "NewUser", createdAt: new Date() }}
+    `(
+      "should handle signIn correctly when $caseName",
+      async ({ username, shouldExist, expectedUserReturn }) => {
+        const mockUserBody = { username }; // Mock the UserBodyDto
+
+        if (shouldExist) {
+          service.findUserByUsername = jest
+            .fn()
+            .mockResolvedValue(expectedUserReturn);
+        } else {
+          service.findUserByUsername = jest.fn().mockResolvedValue(null);
+          service.create = jest.fn().mockResolvedValue(expectedUserReturn);
+        }
+
+        // Mock Request object
+        const mockRequest = {} as any;
+
+        const result = await controller.signIn(mockRequest, mockUserBody);
+        expect(result).toEqual(expectedUserReturn);
+
+        // If the user does not exist, ensure the 'create' method is called
+        if (!shouldExist) {
+          expect(service.create).toHaveBeenCalledWith(
+            mockRequest,
+            mockUserBody
           );
         }
       }
