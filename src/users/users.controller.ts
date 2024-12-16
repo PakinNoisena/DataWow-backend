b8abe7a8984e783b0125673634293652069ea83a
GOLF <pakin.n@muze.co.th>
2024-12-16 20:29:46 +0700

    add: sign in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,11 +5,13 @@ import {
   NotFoundException,
   Param,
   Post,
+  Put,
   Req,
 } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { Request } from "express";
 import { UserEntity } from "../entities/user.entity";
+import { UserBodyDto } from "../dto/users.dto";
 
 @Controller("/users")
 export class UsersController {
@@ -27,6 +29,25 @@ export class UsersController {
         message: `User with username '${username}' not found`,
         messageCode: 1001,
       });
+    }
+
+    return user;
+  }
+
+  @Post("/signin")
+  async signIn(
+    @Req() context: Request,
+    @Body() body: UserBodyDto
+  ): Promise<UserEntity> {
+    // find user to check exist
+    let user = await this.userService.findUserByUsername(
+      context,
+      body.username
+    );
+
+    // create if user not exist
+    if (!user) {
+      user = await this.userService.create(context, body);
     }
 
     return user;
