6f746c165669fa000d245878ba35e8ba3758af3c
GOLF <pakin.n@muze.co.th>
2024-12-16 20:57:42 +0700

    add: community module

---
+++ src/community/community.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CommunityEntity } from "../entities/community.entity";
+import { CommunityController } from "./community.controller";
+import { CommunityService } from "./community.service";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([CommunityEntity])],
+  controllers: [CommunityController],
+  providers: [CommunityService],
+})
+export class CommunityModule {}
