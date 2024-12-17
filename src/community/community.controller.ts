2e28544bcac3ba7ad0d8b174b88c94477c8d2c56
GOLF <pakin.n@muze.co.th>
2024-12-17 22:29:16 +0700

    add: route guard to community controller

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { CommunityService } from "./community.service";
 import { CommunityEntity } from "../entities/community.entity";
+import { CheckUserExistMiddleware } from "../middleware/authorization.middleware";
 
 @Controller("/community")
+@UseGuards(CheckUserExistMiddleware)
 export class CommunityController {
   constructor(private communityService: CommunityService) {}
 
--- src/community/community.module.ts
+++ src/community/community.module.ts
@@ -1,12 +1,21 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CommunityEntity } from "../entities/community.entity";
 import { CommunityController } from "./community.controller";
 import { CommunityService } from "./community.service";
+import { UsersModule } from "../users/users.module";
+import { CheckUserExistMiddleware } from "../middleware/authorization.middleware";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommunityEntity])],
+  imports: [TypeOrmModule.forFeature([CommunityEntity]), UsersModule],
   controllers: [CommunityController],
   providers: [CommunityService],
 })
-export class CommunityModule {}
+export class CommunityModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Apply the CheckUserExistMiddleware only to routes in the CommunityController
+    consumer
+      .apply(CheckUserExistMiddleware) // Registering middleware
+      .forRoutes(CommunityController); // Apply to all routes in this controller
+  }
+}
