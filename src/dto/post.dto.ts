a1fe2abc70a34d8c4e92b1658fe0b56e92ee5540
GOLF <pakin.n@muze.co.th>
2024-12-17 20:59:40 +0700

    add: createPostBodyDto

--- src/dto/post.dto.ts
+++ src/dto/post.dto.ts
@@ -27,3 +27,26 @@ const PostUpdateBodySchema = z.object({
 });
 
 export class PostUpdateBodyDto extends createZodDto(PostUpdateBodySchema) {}
+
+const PostCreateBodySchema = z.object({
+  title: z
+    .string()
+    .min(1, "Username cannot be empty")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Username cannot be null or undefined",
+    }),
+  description: z
+    .string()
+    .min(1, "Description cannot be empty")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Description cannot be null or undefined",
+    }),
+  communityId: z
+    .number()
+    .positive("Community ID must be greater than 0")
+    .refine((value) => value !== null && value !== undefined, {
+      message: "Community ID cannot be null or undefined",
+    }),
+});
+
+export class PostCreateBodyDto extends createZodDto(PostCreateBodySchema) {}
